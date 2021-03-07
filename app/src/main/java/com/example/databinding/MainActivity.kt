7ea2bb3901793952373e2a67c2918bcad6b796dd
ParkSangSun1 <appleasz1028102@gmail.com>
2021-03-07 17:28:13 +0900

    Databinding 기본 사용방법

---
+++ app/src/main/java/com/example/databinding/MainActivity.kt
@@ -0,0 +1,27 @@
+package com.example.databinding
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.databinding.DataBindingUtil
+import com.example.databinding.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var binding : ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+
+        binding.submitButton.setOnClickListener {
+            displayGreeting()
+        }
+    }
+
+    private fun displayGreeting(){
+        binding.apply {
+            greetingTextView.text = "Hello! "+nameEditText.text
+
+        }
+
+    }
+}
